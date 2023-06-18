07bfcd2a2a4fca8a21babad5a510a0547b432263
othmane <othma125@hotmail.com>
2023-06-18 15:06:47 +0100

    betty fixed

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include "monty.h"
 /**
  * main - interpreter function
@@ -6,24 +5,23 @@
  * @argv: arguments set
  * Return: success or failure
  */
-
 int main(int argc, char **argv)
 {
-  FILE *script_file = NULL;
-  int exit_stat = 0;
+	FILE *script_file = NULL;
+	int exit_stat = 0;
 
-  if (argc != 2)
-  {
-    fprintf(stderr, "USAGE: monty file\n");
-    return (1);
-  }
-  script_file = fopen(argv[1], "r");
-  if (!script_file)
-  {
-    fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-    return (1);
-  }
-  exit_stat = monty(script_file);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		return (1);
+	}
+	script_file = fopen(argv[1], "r");
+	if (!script_file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		return (1);
+	}
+	exit_stat = monty(script_file);
 	fclose(script_file);
 	return (exit_stat);
 }
