ec036b8763824e80ab185cb74ee4ed6a1d3be49c
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-20 20:22:49 +0100

    Create math_functions.c

---
+++ math_functions.c
@@ -0,0 +1,99 @@
+#include "monty.h"
+/**
+ * add - add
+ * @stack: stack
+ * @line_number: integer
+ * Return: none
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
+		return;
+	}
+
+	(*stack)->next->next->n += (*stack)->next->n;
+	monty_pop(stack, line_number);
+}
+
+/**
+ * sub - sub
+ * @stack: stack
+ * @line_number: integer
+ * Return: none
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
+		return;
+	}
+
+	(*stack)->next->next->n -= (*stack)->next->n;
+	pop(stack, line_number);
+}
+
+/**
+ * _div - div
+ * @stack: stack
+ * @line_number: integer
+ * Return: none
+ */
+void _div(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		return;
+	}
+
+	if ((*stack)->next->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		return;
+	}
+
+	(*stack)->next->next->n /= (*stack)->next->n;
+	pop(stack, line_number);
+}
+
+/**
+ * mul - mul
+ * @stack: stack
+ * @line_number: integer
+ * Return: none
+ */
+void mul(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		return;
+	}
+
+	(*stack)->next->next->n *= (*stack)->next->n;
+	pop(stack, line_number);
+}
+/**
+ * mod - modulo
+ * @stack: stack
+ * @line_number: integer
+ * Return: none
+ */
+void mod(stack_t **stack, unsigned int line_number)
+{
+	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		return;
+	}
+	if ((*stack)->next->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		return;
+	}
+	(*stack)->next->next->n %= (*stack)->next->n;
+	pop(stack, line_number);
+}
