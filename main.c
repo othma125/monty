ab3737e9e8cf729e149ceabce6712da1bf9fc4aa
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-20 19:55:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,20 @@
 int main(int argc, char **argv)
 {
 	FILE *monty_file = NULL;
-	int exit_stat = 0;
+	int exit_stat;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	monty_file = fopen(argv[1], "r");
 	if (!monty_file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		return (1);
+		return (EXIT_FAILURE);
 	}
 	exit_stat = monty(monty_file);
 	fclose(monty_file);
-	return (1 - exit_stat);
+	return (exit_stat);
 }
