0324d050eb5fac33dde4a91ce42a7bc60a13130e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-21 15:17:01 +0100

    Create func_selector.c

---
+++ func_selector.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * function_selector - select the right function in term of opcode
+ * @opcode: The opcode to match.
+ * Return: function pointer
+ */
+void (*function_selector(char *opcode))(stack_t**, unsigned int)
+{
+	instruction_t functions[] = {
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
+		{"sub", sub},
+		{"div", _div},
+		{"mul", mul},
+		{"mod", mod},
+		{"pchar", pchar},
+		{"pstr", pstr},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{"stack", stack},
+		{"queue", queue},
+		{NULL, NULL}
+	};
+	int i;
+
+	for (i = 0; functions[i].opcode; i++)
+		if (strcmp(opcode, functions[i].opcode) == 0)
+			return (functions[i].f);
+	return (NULL);
+}
