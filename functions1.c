6769f336549220d092ea9731d3087fb9e69bb9d4
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-21 18:40:04 +0100

    Update functions1.c

--- functions1.c
+++ functions1.c
@@ -17,7 +17,7 @@ int push(char **op_toks, stack_t **stack, unsigned int line_number)
 	}
 	if (args_count(op_toks) != 2)
 	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
+		fprintf(stderr, "L%u: unknown instruction %s\n", line_number, op_toks[0]);
 		return (EXIT_FAILURE);
 	}
 	new->n = atoi(op_toks[1]);
