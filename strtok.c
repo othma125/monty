fc019224efe8d47c1c370f3cfdaa845997dc16f8
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-21 15:10:02 +0100

    Update strtok.c

--- strtok.c
+++ strtok.c
@@ -1,5 +1,27 @@
 #include "monty.h"
+/**
+ * readLine - check code
+ * @file: input stream
+ * @line: string to read
+ * Return: string length
+ */
+unsigned int readLine(FILE *file, char **line)
+{
+	unsigned int max_len = 128, len = 0;
+	char ch = getc(file);
 
+	*line = malloc(max_len);
+	if (*line == NULL)
+		return (0);
+	while ((ch != '\n') && (ch != EOF))
+	{
+		(*line)[len] = ch;
+		len++;
+		ch = getc(file);
+	}
+	(*line)[len] = '\0';
+	return (len);
+}
 /**
  * _calloc - allocates memory for an array, using malloc.
  *
