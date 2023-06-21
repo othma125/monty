daec97b2f4dd6fc13f1ed22dc5336d564778981e
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-21 16:17:28 +0100

    Update math_functions.c

--- math_functions.c
+++ math_functions.c
@@ -3,104 +3,102 @@
  * add - add
  * @stack: stack
  * @number: integer
- * Return: none
+ * Return: success
  */
 void add(stack_t **stack, unsigned int number)
 {
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't add, stack too short\n", number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 
 	(*stack)->next->next->n += (*stack)->next->n;
 	pop(stack, number);
+	return (EXIT_SUCCESS);
 }
 
 /**
  * sub - sub
  * @stack: stack
  * @line_number: integer
- * Return: none
+ * Return: success
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't sub, stack too short\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 
 	(*stack)->next->next->n -= (*stack)->next->n;
 	pop(stack, line_number);
+	return (EXIT_SUCCESS);
 }
 
 /**
  * _div - div
  * @stack: stack
  * @line_number: integer
- * Return: none
+ * Return: success
  */
 void _div(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 
 	if ((*stack)->next->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 
 	(*stack)->next->next->n /= (*stack)->next->n;
 	pop(stack, line_number);
+	return (EXIT_SUCCESS);
 }
 
 /**
  * mul - mul
  * @stack: stack
  * @line_number: integer
- * Return: none
+ * Return: success
  */
 void mul(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 
 	(*stack)->next->next->n *= (*stack)->next->n;
 	pop(stack, line_number);
+	return (EXIT_SUCCESS);
 }
 /**
  * mod - modulo
  * @stack: stack
  * @line_number: integer
- * Return: none
+ * Return: success
  */
 void mod(stack_t **stack, unsigned int line_number)
 {
 	if ((*stack)->next == NULL || (*stack)->next->next == NULL)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 	if ((*stack)->next->n == 0)
 	{
 		fprintf(stderr, "L%u: division by zero\n", line_number);
-		success = 0;
-		return;
+		return (EXIT_FAILURE);
 	}
 	(*stack)->next->next->n %= (*stack)->next->n;
 	pop(stack, line_number);
+	return (EXIT_SUCCESS);
 }
