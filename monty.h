70be26d2f4f1597c03fd20c407200aa39a2c2e67
Othmane EL YAAKOUBI <48627755+othma125@users.noreply.github.com>
2023-06-21 15:56:21 +0100

    Update monty.h

--- monty.h
+++ monty.h
@@ -44,7 +44,7 @@ typedef struct instruction_s
 int monty(FILE *);
 char **_strtok(char *);
 void free_stack(stack_t **);
-int create_stack(stack_t **);
+void create_stack(stack_t **);
 void _free(char **);
 unsigned int readLine(FILE *, char **);
 void (*function_selector(char *))(stack_t**, unsigned int);
